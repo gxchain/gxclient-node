b40266bdb92e2c0f9db453d1fa6d71ec66b5b256
lanhaoxiang <lanhaoxiang@qq.com>
2018-10-31 21:04:43 +0800

    feat: add key pair apis

--- test/keyGen.js
+++ test/keyGen.js
@@ -1,4 +1,5 @@
 import {GXClient} from "../lib";
 
 let client = new GXClient();
-console.log(client.generateKey());
+let keypairs = client.generateKey();
+console.log(keypairs, client.privateToPublic(keypairs.privateKey),client.isValidPrivate(keypairs.privateKey), client.isValidPublic(keypairs.publicKey));
