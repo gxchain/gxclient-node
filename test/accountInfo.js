f8079fa7e0e7a4cc961969c57b7b3e38e9c7ba95
lanhaoxiang <lanhaoxiang@qq.com>
2018-10-31 13:33:46 +0800

    feat: add getAccountByPublicKey api and testcase

--- test/accountInfo.js
+++ test/accountInfo.js
@@ -3,4 +3,7 @@ import {GXClient} from "../lib";
 let client = new GXClient();
 client.getAccountBalances("gxb123").then(balances => {
     console.log(balances);
+    client.getAccountByPublicKey("GXC5jynfNTY35xBkFTSKDUfzRrHDmruDCwmcPAU97gyeupKZwfzUh").then(result=>{
+        console.log(result);
+    });
 });
