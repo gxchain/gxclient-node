25f2c1edd95307d67ace5bbc57c64d93fc52ac12
lanhaoxiang <lanhaoxiang@qq.com>
2018-11-06 21:16:54 +0800

    chore: finalize a transaction before serialize

--- test/vote.js
+++ test/vote.js
@@ -3,7 +3,7 @@ import {GXClient} from "../lib";
 const private_key = "";
 const account_id = "1.2.19";
 let client = new GXClient(private_key, account_id, "ws://47.96.164.78:28090");
-client.vote(["1.2.21"], false, "1.3.1", true).then(result => {
+client.vote(["1.2.21"], false, "1.3.1", false).then(result => {
     console.log(JSON.stringify(result, null, "  "));
 }).catch(ex => {
     console.error(ex);
