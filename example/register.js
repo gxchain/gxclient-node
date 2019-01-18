3bae044c8030866e2474f6acc14f4e60b5a2702c
lanhaoxiang <lanhaoxiang@qq.com>
2019-01-18 15:05:35 +0800

    chore: update register example

--- example/register.js
+++ example/register.js
@@ -4,7 +4,7 @@ let client = new GXClient();
 let keyPair = client.generateKey();
 console.log(keyPair);
 
-client.register("testaccount-3", keyPair.publicKey).then(resp => {
+client.register("testaccount-3", keyPair.publicKey,"","","https://testnet.faucet.gxchain.org").then(resp => {
     console.log(JSON.stringify(resp));
 }).catch(ex => {
     console.error("register failed:", ex);
