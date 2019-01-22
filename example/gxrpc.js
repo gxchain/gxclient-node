59d86f2d898a20d28299f519b73672a6c495324d
lanhaoxiang <lanhaoxiang@qq.com>
2019-01-22 14:23:43 +0800

    chore: optimize gxrpc error handler

--- example/gxrpc.js
+++ example/gxrpc.js
@@ -4,5 +4,5 @@ let rpc = GXRPC.instance("https://node1.gxb.io");
 rpc.query("get_chain_id", []).then(resp => {
     console.log(resp);
 }).catch(ex => {
-    console.error(ex);
+    console.error("error", ex);
 });
