0404c2264d77534c1459e3859a1fe72e332c1d53
lanhaoxiang <lanhaoxiang@qq.com>
2019-12-04 16:06:54 +0800

    chore: remove dist and es files

--- es/const/const.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.DEFUALT_EXPIRE_SEC = void 0;
-var DEFUALT_EXPIRE_SEC = 180;
-exports.DEFUALT_EXPIRE_SEC = DEFUALT_EXPIRE_SEC;
